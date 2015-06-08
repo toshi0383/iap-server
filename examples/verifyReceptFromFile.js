6292275e74706989c17f86eac9e0a2aa63c9a715
toshi0383 <t.suzuki326@gmail.com>
2015-06-08 13:42:23 +0900

    add example

---
+++ examples/verifyReceptFromFile.js
@@ -0,0 +1,8 @@
+var receipt = process.argv[2]
+var Validation= require('./Validation.js');
+
+var validation = new Validation();
+validation.check(receipt, function (data, isValid) {
+  console.log('is valid :  ' + isValid);
+  console.log('receive data :  ' + data);
+});
