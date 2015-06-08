2cf0bdc888d8211f3e2e5b27b6995e339f3aaadb
toshi0383 <t.suzuki326@gmail.com>
2015-06-08 14:00:22 +0900

    prod => sandbox

--- Validation.js
+++ Validation.js
@@ -28,21 +28,36 @@ var post = function (url, data, cb) {
   req.end();
 };
 
-//the sandbox url: 
+//the sandbox url:
 var sandboxURL = 'https://sandbox.itunes.apple.com/verifyReceipt';
 //the production url:
-//var prodURL = 'https://buy.itunes.apple.com/verifyReceipt';
+var prodURL = 'https://buy.itunes.apple.com/verifyReceipt';
+
+function checkResultStatus(status, data, callback) {
+  if (status == 0) {
+    callback(data, true);
+  } else {
+    callback(data, false);
+  }
+}
 
 Validation.prototype.check = function(receipt,callback){
 	var content = '{"receipt-data" : "' + receipt + '"}';
-  post(sandboxURL, content, function (data) {
+  post(prodURL, content, function (data) {
     var json = JSON.parse(data);
-    if (json.status == 0) {
-      callback(data, true);
-    }else{
-      callback(data, false);
+    var status = json.status
+    if (status == 21007) {
+      // receipt is from the test environment,
+      // but it was sent to production environment.
+      post(sandboxURL, content, function(data) {
+        var json = JSON.parse(data);
+        checkResultStatus(status, data, callback);
+      })
+      return
     }
+    checkResultStatus(status, data, callback);
   });
 };
+
 
 module.exports = Validation;
